4f7df3dd278195952da9527e8bb08198a750abf0
pau <paugitonga6@gmail.com>
2022-06-10 21:03:33 +0300

    another commit

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee=
+{
+name:'Paulina',
+streetAddress:' Mirema1stAvenue',
+};
+function updateEmployeeWithKeyAndValue(employee,key,value)
+{
+    return Object.assign ({}, employee, { [key]: value })
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+  
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    // Alternate using ES6 Spread operators:
+    // const newObj = { ...employee }
+    const newObj = Object.assign({}, employee);
+  
+    delete newObj[key];
+  
+    return newObj;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(driver, key) {
+    delete employee[key];
+  
+    return employee;
+  }
